6d7befb186b28c698b6fa89f0545f22adbcc308e
Rao Atif <ratifba86@gmail.com>
2024-01-29 10:09:00 +0500

    as

---
+++ script.js
@@ -0,0 +1,14 @@
+document.addEventListener('mousemove', (e) => {
+	const cursor = document.querySelector('.cursor');
+	cursor.style.transform = `translate(${e.clientX}px, ${e.clientY}px)`;
+});
+
+document.getElementById('view-btn').addEventListener('click', () => {
+	const details = document.querySelector('.details');
+	details.style.display = 'block';
+});
+
+document.getElementById('close-btn').addEventListener('click', () => {
+	const details = document.querySelector('.details');
+	details.style.display = 'none';
+});
